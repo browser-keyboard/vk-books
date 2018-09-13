794023be92f15789137ac8ddf62ae33ced9cceab
Ruslan Sayfullin <uraltus@yandex.ru>
2018-09-13 18:17:33 +0500

    seperate settings and init database settings

---
+++ project/settings/local.py
@@ -0,0 +1,14 @@
+from project.settings.base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
---
+++ project/settings/production.py
@@ -0,0 +1,12 @@
+from project.settings.base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
+        'PORT': os.environ.get('DATABASE_PORT', '5432'),
+    }
+}
