65c61ac48561b078a85f2db8be6f5f0882d9b54a
Ruslan Sayfullin <uraltus@yandex.ru>
2018-10-28 06:06:40 +0500

    basic accounts models init

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    class VISIBILITY_TYPE:
+        ALL_FRIENDS = 0
+        ONLY_OWNER = 1
+        ONLY_SOME_FRIENDS = 2
+        ONLY_SOME_FRIENDS_LISTS = 3
+    VISIBILITY_TYPE_CHOICES = (
+        (VISIBILITY_TYPE.ALL_FRIENDS, 'all friends'),
+        (VISIBILITY_TYPE.ONLY_OWNER, 'all friends'),
+        (VISIBILITY_TYPE.ONLY_SOME_FRIENDS, 'only some friends'),
+        (VISIBILITY_TYPE.ONLY_SOME_FRIENDS_LISTS, 'only some friends lists'),
+    )
+
+    vk_id = models.CharField(max_length=255)
+    visibility_type = models.SmallIntegerField(choices=VISIBILITY_TYPE_CHOICES, default=VISIBILITY_TYPE.ALL_FRIENDS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class WhiteListOfFriendsLists(models.Model):
+    owner_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    friend_id = models.CharField(max_length=255)
+
+
+class WhiteListOfLists(models.Model):
+    owner_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    friend_list_id = models.CharField(max_length=255)
--- project/settings/base.py
+++ project/settings/base.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+sys.path.insert(0, os.path.join(BASE_DIR, '../apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -37,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
+    'books',
+    'frontend',
 ]
 
 MIDDLEWARE = [
